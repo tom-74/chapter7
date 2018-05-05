fae5cfdca50e7a5012f2c5638bb055480418f030
tom <tom>
2018-05-05 11:07:07 +0800

    Adding nwe modules

--- modules/dislister.py
+++ modules/dislister.py
@@ -1,5 +1,5 @@
 import os
 def run(**args):
-print "[*] In dirlister module."
-files=os.listdir(".")
-return str(files)
+	print "[*] In dirlister module."
+	files=os.listdir(".")
+	return str(files)
